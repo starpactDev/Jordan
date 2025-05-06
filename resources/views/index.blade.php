977bcee756fc5822134c3d8db7b5ea86a8d30cf2
starpactDev <developers.starpact@gmail.com>
2025-05-06 17:00:19 +0530

    Add header navigation and logo to index view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -10,6 +10,26 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
-    <h1>Hello World</h1>
+    <header class="hidden lg:block bg-white dark:bg-violet-950 dark:text-white sticky top-0  h-20 ">
+        <div class="grid grid-cols-12 gap-4 items-center h-full container mx-auto">
+            <div class="col-span-2">
+                <a href="">
+                    <img src="{{ asset('/images/logo/puva-black.png') }}" alt="">
+                </a>
+            </div>
+            <div class="col-span-8">
+                <nav class="flex justify-center items-center gap-4">
+                    <a href="" class="text-gray-500 hover:text-gray-900">Home</a>
+                    <a href="" class="text-gray-500 hover:text-gray-900">About</a>
+                    <a href="" class="text-gray-500 hover:text-gray-900">Services</a>
+                    <a href="" class="text-gray-500 hover:text-gray-900">Contact</a>
+                </nav>
+            </div>
+            <div class="col-span-2">
+                <a href="" class="bg-violet-950 dark:border hover:bg-violet-600 text-white font-semibold tracking-wide px-8 py-4 rounded-full transition-colors duration-700 ease-in-out">Submit Review </a>
+            </div>
+        </div>
+    </header>
+    <section class="min-h-[200vh] bg-amber-50" ></section>
 </body>
 </html>
